1cee57779e05955c7c1cac649b1f9bb4487e950e
psrisaiachyutha <99003269+psrisaiachyutha@users.noreply.github.com>
2022-02-10 13:28:22 +0530

    modified test cases for course details

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('HUEdgeAngular-Assignment');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('HUEdgeAngular-Assignment app is running!');
-  });
+  
 });
