ad1aaffc1d1ce5bc413385f321ca355d2398bdb1
psrisaiachyutha <99003269+psrisaiachyutha@users.noreply.github.com>
2022-02-10 10:29:56 +0530

    added test cases for banner component

--- src/app/components/banner/banner.component.spec.ts
+++ src/app/components/banner/banner.component.spec.ts
@@ -22,4 +22,14 @@ describe('BannerComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should take given string as input', () => {
+    component.data = "this is angular banner";
+    fixture.detectChanges();
+    var paragraph = fixture.nativeElement.querySelector('p');
+    expect(paragraph.className).toEqual('mt-3');
+    expect(paragraph.innerHTML).toBe("this is angular banner")
+  });
+
+
 });
